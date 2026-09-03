bc54c3137611620ff32106b17be17a8da6be0887
Barbara <barbara.oktoi@student.moringaschool.com>
2026-09-03 23:48:01 +0300

    Updated bank

--- PYTHON/Intro/object_oriented_programming/bank.py
+++ PYTHON/Intro/object_oriented_programming/bank.py
@@ -5,27 +5,39 @@ deposits, withdral,
 show account.
 getter and setter. ->
 -> easy to scale function <understing>
-___________________________________________
+
 -> Login account
 -> Create account
----> Deposit
----> Withdrawal
----> account balance
-
+-> Deposit
+-> Withdrawal
+-> account balance
 """
 
 class BankAccount:
 
-    def __init__(self,name,balance,account_no):
-        self.name=name
-        self.balance=balance
-        self.account_no=account_no
+    def __init__(self, name, balance, account_no):
+        self.name = name
+        self._balance = balance
+        self.account_no = account_no
 
-    #latter change gee
-    def get_balance(self):
-        pass
+    # data i read
+    @property
+    def balance(self):
+        print("somebody tried to read johns balance")
+        return self._balance
 
-    #setter
+    # to control updated
+    @balance.setter
+    def balance(self, value):
+        if not isinstance(value, (int, float)):
+            print("Ensure you pass a number for new balance")
+            return
+        if value < 0:
+            print("Ensure new balance must not be less than 0")
+            return
+        self._balance = value
+
+    # setter
     def deposit(self):
         pass
 
@@ -37,6 +49,10 @@ class BankAccount:
         print(f"Balance {self.balance}")
         print(f"Account No {self.account_no}")
 
-john=BankAccount(name="John Mwangi",balance=0,account_no="223344223")
 
-john.show_account_details()
+john = BankAccount(name="John Mwangi", balance=0, account_no="223344223")
+
+print("John blance ", john.balance)
+# john.show_account_details()
+# it is easily accessable ->
+# john.balance=-5000 #setting - undating the property
